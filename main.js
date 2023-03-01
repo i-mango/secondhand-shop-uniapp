4bebabc2020f17ad220713130cdc00703f8ba96d
潘纪婷 <mango@imufeng.cn>
2023-03-01 18:03:18 +0800

    初始化项目

--- main.js
+++ main.js
@@ -1,29 +1,50 @@
 import App from './App'
-import uView from 'uview-ui'
+
+// #ifndef VUE3
 import Vue from 'vue'
-import store from 'store/index.js'
-import {router,RouterMount} from 'router/router.js'
-import showModal from "components/login_tip.vue"
-import agreement from "components/agreement.vue"
-import updatevalue from "components/updateInfo.vue"
-import initModal from "components/login_Controll.js"
-import {request} from "config/request.js"
-Vue.use(router)
-initModal(Vue);
-Vue.component('show-modal',showModal);
-Vue.component('agreement',agreement);
-Vue.component('updatevalue',updatevalue);
-Vue.use(uView);
-Vue.prototype.$store = store
 Vue.config.productionTip = false
-Vue.prototype.$request=request
 App.mpType = 'app'
+
+try {
+  function isPromise(obj) {
+    return (
+      !!obj &&
+      (typeof obj === "object" || typeof obj === "function") &&
+      typeof obj.then === "function"
+    );
+  }
+
+  // 统一 vue2 API Promise 化返回格式与 vue3 保持一致
+  uni.addInterceptor({
+    returnValue(res) {
+      if (!isPromise(res)) {
+        return res;
+      }
+      return new Promise((resolve, reject) => {
+        res.then((res) => {
+          if (res[0]) {
+            reject(res[0]);
+          } else {
+            resolve(res[1]);
+          }
+        });
+      });
+    },
+  });
+} catch (error) { }
+
 const app = new Vue({
-    ...App,
-	 store
+  ...App
 })
 app.$mount()
+// #endif
 
-
-
-
+// #ifdef VUE3
+import { createSSRApp } from 'vue'
+export function createApp() {
+  const app = createSSRApp(App)
+  return {
+    app
+  }
+}
+// #endif
