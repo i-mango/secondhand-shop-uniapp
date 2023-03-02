dcefcedd99001232dca53b211ac1acdd88c04116
潘纪婷 <mango@imufeng.cn>
2023-03-02 17:22:09 +0800

    初始化项目

---
+++ pages/index/index.vue
@@ -0,0 +1,52 @@
+<template>
+	<view class="content">
+		<image class="logo" src="/static/logo.png"></image>
+		<view class="text-area">
+			<text class="title">{{title}}</text>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200px;
+		width: 200px;
+		margin-top: 200px;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50px;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36px;
+		color: #8f8f94;
+	}
+</style>
---
+++ pages/own/index.vue
@@ -0,0 +1,12 @@
+<template>
+	<button class="own">个人中心</button>
+</template>
+
+<script>
+</script>
+
+<style>
+	.own{
+		border: 1px solid royalblue;
+	}
+</style>
